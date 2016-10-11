35eb445065fc584887d8986d5ae6a01515a9a037
Tommy Huff <tuplink@gmail.com>
2016-10-11 00:42:22 -0400

    fix SU User

--- modules/dynamicdns.duckdns.sh
+++ modules/dynamicdns.duckdns.sh
@@ -3,17 +3,18 @@ dynamic_dns_duckdns(){
     if [ "$DYNDNS" != "$(date +%H)" ] ; then
       INFO "Updating Dynamic DNS"
       DYNDNS=$(date +%H)
-      DNS=$(su $RTORRENTUSER -c "echo -e 'GET http://www.duckdns.org/update?domains=$DUCKDOMAIN&token=$DUCKKEY&ip= HTTP/1.0\n\n' | nc -w 2 www.duckdns.org 80")
+      DNS=$(su $TORRENTUSER -c "echo -e 'GET http://www.duckdns.org/update?domains=$DUCKDOMAIN&token=$DUCKKEY&ip= HTTP/1.0\n\n' | nc -w 2 www.duckdns.org 80")
     fi
     if [ -z $PUBLICIP ] ; then
       PUBLICIP=$DUCKDOMAIN
     fi
   else
-    ERROR "DUCKDOMAIN and/or DUCKKEY not set"
+    ERROR "DUCKDOMAIN, DUCKKEY and/or TORRENTUSER not set"
     exit 1
   fi
 }
 if [ "$1" == "help" ] ; then
   echo "Must set DUCKKEY= in Config"
   echo "Must set DUCKDOMAIN= in Config"
+  echo "Must set TORRENTUSER= in config"
 fi
