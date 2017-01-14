807f61efc6fb14008448d4e151ee948029b14d14
Tommy Huff <tuplink@gmail.com>
2017-01-14 23:37:48 +0000

    try to get condition correct for auto re throw

--- modules/forward.pia.sh
+++ modules/forward.pia.sh
@@ -2,30 +2,28 @@
 forward_pia(){
   DEBUG "Checking if we need to run port forward script"
   if [ -n $VPNIF ] && [ -n $VPNPASS ] ; then
-    if [ "$PORTFOR" != $(date +%H) ] ; then
-      if [ ${MONITOR[VPN Internet]} -ge 3 ] ; then
-        PORT=$($SELFDIR/portforward/port_forward.sh -f $VPNPASS -i $VPNIF -s)
-        if [[ $PORT =~ ^-?[0-9]+$ ]] ; then
-          PORTFOR=$(date +%H)
-          DEBUG "Inbound port is $PORT"
-          if [ "$PORT" != "$OLDPORT" ] || [ -z $OLDPORT ] ; then
-            INFO "Port Number changed"
-            DEBUG "Adding new rule for inbound port"
-            iptables -D INPUT -i $VPNIF -j DROP
-            if [ -n "$OLDPORT" ] ; then
-              iptables -t nat -D PREROUTING -i $VPNIF -p tcp --dport $OLDPORT -j REDIRECT --to-port 80
-            fi
-            iptables -t nat -A PREROUTING -i $VPNIF -p tcp --dport $PORT -j REDIRECT --to-port 80
-            iptables -A INPUT -i $VPNIF -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-            iptables -A INPUT -i $VPNIF -j DROP
-            OLDPORT=$PORT
-            send_msg "http://$VPNDNS:$PORT Forwarded to 80"
-          else
-            INFO "Port is same as old"
+    if [ "$PORTFOR" != $(date +%H) ] || [ ${MONITOR[VPN Internet]} -ge 3 ] ; then
+      PORT=$($SELFDIR/portforward/port_forward.sh -f $VPNPASS -i $VPNIF -s)
+      if [[ $PORT =~ ^-?[0-9]+$ ]] ; then
+        PORTFOR=$(date +%H)
+        DEBUG "Inbound port is $PORT"
+        if [ "$PORT" != "$OLDPORT" ] || [ -z $OLDPORT ] ; then
+          INFO "Port Number changed"
+          DEBUG "Adding new rule for inbound port"
+          iptables -D INPUT -i $VPNIF -j DROP
+          if [ -n "$OLDPORT" ] ; then
+            iptables -t nat -D PREROUTING -i $VPNIF -p tcp --dport $OLDPORT -j REDIRECT --to-port 80
           fi
+          iptables -t nat -A PREROUTING -i $VPNIF -p tcp --dport $PORT -j REDIRECT --to-port 80
+          iptables -A INPUT -i $VPNIF -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+          iptables -A INPUT -i $VPNIF -j DROP
+          OLDPORT=$PORT
+          send_msg "http://$VPNDNS:$PORT Forwarded to 80"
         else
-          INFO "Port forward enable failed"
+          INFO "Port is same as old"
         fi
+      else
+        INFO "Port forward enable failed"
       fi
     fi
   else
